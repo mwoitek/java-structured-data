cf4dbcaf6e0de3a2866a264e4fcc5b5d6a5cd179
Marcio Woitek <woitek@usp.br>
2021-07-22 02:53:10 -0300

    Completed 1st assignment (Implementation of encryptTwoKeys)

--- week_1/CaesarCipher.java
+++ week_1/CaesarCipher.java
@@ -39,17 +39,18 @@ class CaesarCipher {
     return alphabet.substring(key) + alphabet.substring(0, key);
   }
 
-  public static void printBoolean(boolean bool) {
-    System.out.println(Boolean.toString(bool));
-  }
-
   public static void testGetShiftedAlphabet() {
     String alphabet = getAlphabet();
-    printBoolean(getShiftedAlphabet(alphabet, 1).equals("BCDEFGHIJKLMNOPQRSTUVWXYZA"));
-    printBoolean(getShiftedAlphabet(alphabet, 2).equals("CDEFGHIJKLMNOPQRSTUVWXYZAB"));
-    printBoolean(getShiftedAlphabet(alphabet, 4).equals("EFGHIJKLMNOPQRSTUVWXYZABCD"));
-    printBoolean(getShiftedAlphabet(alphabet, 8).equals("IJKLMNOPQRSTUVWXYZABCDEFGH"));
-    printBoolean(getShiftedAlphabet(alphabet, 16).equals("QRSTUVWXYZABCDEFGHIJKLMNOP"));
+    WordPlay.printBoolean(
+        getShiftedAlphabet(alphabet, 1).equals("BCDEFGHIJKLMNOPQRSTUVWXYZA"));
+    WordPlay.printBoolean(
+        getShiftedAlphabet(alphabet, 2).equals("CDEFGHIJKLMNOPQRSTUVWXYZAB"));
+    WordPlay.printBoolean(
+        getShiftedAlphabet(alphabet, 4).equals("EFGHIJKLMNOPQRSTUVWXYZABCD"));
+    WordPlay.printBoolean(
+        getShiftedAlphabet(alphabet, 8).equals("IJKLMNOPQRSTUVWXYZABCDEFGH"));
+    WordPlay.printBoolean(
+        getShiftedAlphabet(alphabet, 16).equals("QRSTUVWXYZABCDEFGHIJKLMNOP"));
   }
 
   public static String encrypt(String input, int key) {
@@ -77,24 +78,61 @@ class CaesarCipher {
     return encryptedInput.toString();
   }
 
-  public static void encryptTextFile(String fileName, int key) {
+  public static String encryptTwoKeys(String input, int key1, int key2) {
+    char ch;
+    char chShifted;
+    int idx;
+
+    String alphabet = getAlphabet();
+    String shiftedAlphabet1 = getShiftedAlphabet(alphabet, key1);
+    String shiftedAlphabet2 = getShiftedAlphabet(alphabet, key2);
+    StringBuilder encryptedInput = new StringBuilder();
+
+    for (int i = 0; i < input.length(); i++) {
+      ch = input.charAt(i);
+      idx = alphabet.indexOf(Character.toUpperCase(ch));
+
+      if (idx != -1) {
+        chShifted = WordPlay.isEven(i) ? shiftedAlphabet1.charAt(idx)
+                                       : shiftedAlphabet2.charAt(idx);
+        encryptedInput.append(Character.isLowerCase(ch) ? Character.toLowerCase(chShifted)
+                                                        : chShifted);
+      } else {
+        encryptedInput.append(ch);
+      }
+    }
+
+    return encryptedInput.toString();
+  }
+
+  public static void encryptTextFile(String fileName, Integer key1, Integer key2) {
     String message = readTextFile(fileName);
-    String encryptedMessage = encrypt(message, key);
+    String encryptedMessage =
+        key2 == null ? encrypt(message, key1.intValue())
+                     : encryptTwoKeys(message, key1.intValue(), key2.intValue());
 
     System.out.println("Original message:");
     System.out.println(message);
 
     System.out.println();
 
-    System.out.println("Encrypted message (key = " + String.valueOf(key) + "):");
+    String strKeys = key2 == null
+                         ? "key = " + key1.toString()
+                         : "key1 = " + key1.toString() + ", key2 = " + key2.toString();
+    System.out.println("Encrypted message (" + strKeys + "):");
     System.out.println(encryptedMessage);
   }
 
   public static void testCaesar() {
     String fileName = "test_caesar.txt";
-    encryptTextFile(fileName, 17);
+
+    encryptTextFile(fileName, Integer.valueOf(17), null);
     System.out.println();
-    encryptTextFile(fileName, 23);
+
+    encryptTextFile(fileName, Integer.valueOf(23), null);
+    System.out.println();
+
+    encryptTextFile(fileName, Integer.valueOf(23), Integer.valueOf(17));
   }
 
   public static void main(String[] args) {
