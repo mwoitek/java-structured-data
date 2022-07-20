923ce25bb541aaf2a0ec550e8138cbfe330337a8
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-20 17:41:44 -0300

    Week 3: Part 2

--- week_3/src/solution/LogAnalyzer.java
+++ week_3/src/solution/LogAnalyzer.java
@@ -26,4 +26,52 @@ public class LogAnalyzer {
     }
   }
 
+  public int countUniqueIPs() {
+    String ipAddress;
+    ArrayList<String> uniqueIPs = new ArrayList<String>();
+    for (LogEntry le : records) {
+      ipAddress = le.getIpAddress();
+      if (!uniqueIPs.contains(ipAddress)) {
+        uniqueIPs.add(ipAddress);
+      }
+    }
+    return uniqueIPs.size();
+  }
+
+  public void printAllHigherThanNum(int num) {
+    for (LogEntry le : records) {
+      if (le.getStatusCode() > num) {
+        System.out.println(le);
+      }
+    }
+  }
+
+  public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+    String day;
+    String ipAddress;
+    ArrayList<String> uniqueIPs = new ArrayList<String>();
+    for (LogEntry le : records) {
+      day = le.getAccessTime().toString().substring(4, 10);
+      ipAddress = le.getIpAddress();
+      if (day.equals(someday) && !uniqueIPs.contains(ipAddress)) {
+        uniqueIPs.add(ipAddress);
+      }
+    }
+    return uniqueIPs;
+  }
+
+  public int countUniqueIPsInRange(int low, int high) {
+    String ipAddress;
+    int statusCode;
+    ArrayList<String> uniqueIPs = new ArrayList<String>();
+    for (LogEntry le : records) {
+      ipAddress = le.getIpAddress();
+      statusCode = le.getStatusCode();
+      if (statusCode >= low && statusCode <= high && !uniqueIPs.contains(ipAddress)) {
+        uniqueIPs.add(ipAddress);
+      }
+    }
+    return uniqueIPs.size();
+  }
+
 }
--- week_3/src/solution/Tester.java
+++ week_3/src/solution/Tester.java
@@ -1,5 +1,6 @@
 package solution;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Tester {
@@ -17,10 +18,71 @@ public class Tester {
     la.printAll();
   }
 
+  public void testUniqueIP() {
+    LogAnalyzer la = new LogAnalyzer();
+    la.readFile("../log/short-test_log");
+    System.out.println("Number of unique IP addresses: " + la.countUniqueIPs());
+  }
+
+  public void testPrintAllHigherThanNum() {
+    LogAnalyzer la = new LogAnalyzer();
+    int num = 300;
+    la.readFile("../log/short-test_log");
+    System.out.println("num = " + num);
+    la.printAllHigherThanNum(num);
+  }
+
+  public void testUniqueIPVisitsOnDay() {
+    LogAnalyzer la = new LogAnalyzer();
+    la.readFile("../log/weblog-short_log");
+    String someday = "Sep 14";
+    ArrayList<String> uniqueIPs = la.uniqueIPVisitsOnDay(someday);
+    System.out.println("someday = " + someday);
+    System.out.println("Number of unique IP addresses: " + uniqueIPs.size());
+    someday = "Sep 30";
+    uniqueIPs = la.uniqueIPVisitsOnDay(someday);
+    System.out.println("someday = " + someday);
+    System.out.println("Number of unique IP addresses: " + uniqueIPs.size());
+  }
+
+  public void testCountUniqueIPsInRange() {
+    LogAnalyzer la = new LogAnalyzer();
+    la.readFile("../log/short-test_log");
+    int low = 200;
+    int high = 299;
+    System.out.println("low = " + low + ", high = " + high);
+    System.out.println("Number of unique IP addresses: " + la.countUniqueIPsInRange(low, high));
+    low = 300;
+    high = 399;
+    System.out.println("low = " + low + ", high = " + high);
+    System.out.println("Number of unique IP addresses: " + la.countUniqueIPsInRange(low, high));
+  }
+
   public static void main(String[] args) {
     Tester t = new Tester();
+
+    System.out.println("Output of 'testLogEntry':");
     t.testLogEntry();
+    System.out.println();
+
+    System.out.println("Output of 'testLogAnalyzer':");
     t.testLogAnalyzer();
+    System.out.println();
+
+    System.out.println("Output of 'testUniqueIP':");
+    t.testUniqueIP();
+    System.out.println();
+
+    System.out.println("Output of 'testPrintAllHigherThanNum':");
+    t.testPrintAllHigherThanNum();
+    System.out.println();
+
+    System.out.println("Output of 'testUniqueIPVisitsOnDay':");
+    t.testUniqueIPVisitsOnDay();
+    System.out.println();
+
+    System.out.println("Output of 'testCountUniqueIPsInRange':");
+    t.testCountUniqueIPsInRange();
   }
 
 }
