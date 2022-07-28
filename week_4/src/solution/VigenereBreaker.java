d604ff6f1215b4b687ae75b34c6a2cfc75af2ba8
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-28 00:15:51 -0300

    Part 1: First two methods

--- week_4/src/solution/VigenereBreaker.java
+++ week_4/src/solution/VigenereBreaker.java
@@ -1,20 +1,61 @@
 package solution;
 
+import edu.duke.FileResource;
+
 public class VigenereBreaker {
 
   public String sliceString(String message, int whichSlice, int totalSlices) {
-    //
-    return "WRITE ME!";
+    StringBuilder sb = new StringBuilder();
+    for (int i = whichSlice; i < message.length(); i += totalSlices) {
+      sb.append(message.charAt(i));
+    }
+    return sb.toString();
+  }
+
+  public void testSliceString() {
+    System.out.println(sliceString("abcdefghijklm", 0, 3)); // adgjm
+    System.out.println(sliceString("abcdefghijklm", 1, 3)); // behk
+    System.out.println(sliceString("abcdefghijklm", 2, 3)); // cfil
+    System.out.println(sliceString("abcdefghijklm", 0, 4)); // aeim
+    System.out.println(sliceString("abcdefghijklm", 1, 4)); // bfj
+    System.out.println(sliceString("abcdefghijklm", 2, 4)); // cgk
+    System.out.println(sliceString("abcdefghijklm", 3, 4)); // dhl
+    System.out.println(sliceString("abcdefghijklm", 0, 5)); // afk
+    System.out.println(sliceString("abcdefghijklm", 1, 5)); // bgl
+    System.out.println(sliceString("abcdefghijklm", 2, 5)); // chm
+    System.out.println(sliceString("abcdefghijklm", 3, 5)); // di
+    System.out.println(sliceString("abcdefghijklm", 4, 5)); // ej
   }
 
   public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
     int[] key = new int[klength];
-    //
+    CaesarCracker cc = new CaesarCracker(mostCommon);
+    String slice;
+    for (int i = 0; i < klength; i++) {
+      slice = sliceString(encrypted, i, klength);
+      key[i] = cc.getKey(slice);
+    }
     return key;
   }
 
+  public void testTryKeyLength() {
+    FileResource fr = new FileResource("../txt/athens_keyflute.txt");
+    String encrypted = fr.asString();
+    int[] key = tryKeyLength(encrypted, 5, 'e');
+    System.out.println("Key:");
+    for (int i = 0; i < 5; i++) {
+      System.out.println(key[i]);
+    }
+  }
+
   public void breakVigenere() {
-    //
+    // HERE
+  }
+
+  public static void main(String[] args) {
+    VigenereBreaker vb = new VigenereBreaker();
+    // vb.testSliceString();
+    vb.testTryKeyLength();
   }
 
 }
